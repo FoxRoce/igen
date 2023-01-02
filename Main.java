81cbd5e1ffff97bc6ba43ef6199d8c1183261e24
RichardToth992 <riprichie32@gmail.com>
2023-01-02 10:31:11 +0100

    maxFinder

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
-<<<<<<< HEAD
+
 
     public static int maxFinder(int[] tomb){
         int max = 0;
@@ -12,10 +12,7 @@ public class Main {
     }
     public static void main(String[] args) {
 
-=======
-    public static void main(String[] args) {
-
         System.out.println("HEllO");
->>>>>>> origin/main
+
     }
 }
