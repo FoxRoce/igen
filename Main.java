1a3ba7fcdfeb8b6aba0411977b9a288d63d96f03
FoxRoce <vegh220@gmail.com>
2023-01-02 09:55:32 +0100

    test

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static int maxFinder(int[] tomb){
+        int max = 0;
+        for (int i = 0; i < tomb.length; i++) {
+           if (tomb[i] > max){
+               max = tomb[i];
+           }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+
+    }
+}
